6cf29652cf33f3ce8a0c4b30b5bb9ef5b61f1b41
Srikakulapu Koteswara Rao <123052136+srikakulapu96@users.noreply.github.com>
2026-01-21 16:43:08 +0530

    Update index.js

--- src/components/destinationSearch/index.js
+++ src/components/destinationSearch/index.js
@@ -19,7 +19,8 @@ class DestinationSeach extends Component {
       <div className="main-bg">
         <div className="search-container">
           <div>
-            <h1 className="heading">Destination Search </h1>
+            <h1 className="heading">Destination Search Edited by Vijju  </h1>
+            <h2>Find your favorite destinations</h2>
           </div>
           <div className="search-input-container">
             <input
